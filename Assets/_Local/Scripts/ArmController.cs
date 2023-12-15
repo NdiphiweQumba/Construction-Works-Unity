978c0b1744cd0cd5894f82b9dc431a4075ca59df
Ndiphiwe Qumba <Ndiphiwecaeser@gmail.com>
2023-12-15 14:32:46 +0200

    Assets | local | Scenes updated values on the bulldozer 360 - 5 minMax && Script update

--- Assets/_Local/Scripts/ArmController.cs
+++ Assets/_Local/Scripts/ArmController.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArmController : MonoBehaviour
 {
-	#region Public Fields
-	public float rotationSpeed = 1.0f;
-	public float maxConstraint = 0.0f;
-	public float minConstraint = 5.0f;
+    #region Public Fields
+    public float rotationSpeed = 1.0f;
+    public float maxConstraint = 0.0f;
+    public float minConstraint = 5.0f;
     #endregion
     #region Private Fields
     #endregion
@@ -23,11 +21,20 @@ public class ArmController : MonoBehaviour
     protected virtual void MoveArm(float input)
     {
         // Rotate the arm around its local x-axis
-        float rotationAmount = input * rotationSpeed * Time.deltaTime;
-        float newRotation = Mathf.Clamp(transform.localEulerAngles.x + 1,minConstraint, maxConstraint);
-        Debug.Log(newRotation);
-        // Apply constraints
-        transform.localEulerAngles = new Vector3(newRotation, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        float rotationAmount =  input * rotationSpeed * Time.deltaTime;
+
+        Debug.Log("Rotaion Amount: " + rotationAmount);
+
+        float xRot = transform.localEulerAngles.x;
+        Debug.Log("x rotation: " + xRot);
+
+        float yRot = transform.localEulerAngles.y;
+        float zRot = transform.localEulerAngles.z;
+
+        if (input > 0 && xRot <= maxConstraint)
+            transform.localEulerAngles = new Vector3(xRot - rotationAmount, yRot, zRot);
+        if(input < 0 && xRot > minConstraint)
+            transform.localEulerAngles = new Vector3(xRot - rotationAmount, yRot, zRot);
     }
     #endregion
 }
